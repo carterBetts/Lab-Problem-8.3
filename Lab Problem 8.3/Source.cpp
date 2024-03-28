0feb51af264aa270c8d29675d32264c5ed560f62
Carter Betts <c-betts@onu.edu>
2024-03-28 09:41:42 -0400

    completed convertRomanToInt function

--- Lab Problem 8.3/Source.cpp
+++ Lab Problem 8.3/Source.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <String>
+#include <list>
 using namespace std;
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
+
+
 
 int main(void)
 {
-	cout << romanCharValue('C');
+	cout << convertRomanToInt("X");
 }
 
 int romanCharValue(char r)
@@ -29,3 +34,38 @@ int romanCharValue(char r)
 	}
 	return 0;
 }
+
+int convertRomanToInt(string s)
+{
+	const char* str = s.c_str();
+	list <char> charecters;
+	for (int i = 0; i < s.length(); i++)
+		charecters.push_back(str[0]);
+
+	int total = 0;
+	while (s.length() > 0)
+	{
+		if ((romanCharValue(s[0]) >= romanCharValue(s[1])) || (s.length() == 1))
+		{
+			total += romanCharValue(s[0]);
+			s.erase(0,1);
+		}
+		else
+		{
+			total += romanCharValue(s[1]) - romanCharValue(s[0]);
+			s.erase(1,1);
+			s.erase(0,1);
+		}
+	}
+	return total;
+}
+
+//total = 0
+//While the roman number string is not empty
+//If the first character has a larger or equal value than the second,
+//or the string has length 1
+//Add value(first character) to total.
+//Remove the character.
+//Else
+//Add the quantity : value(second character) - value(first character) to total.
+//Remove both characters
